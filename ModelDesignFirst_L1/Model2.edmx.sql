05a7e754d9880b385b6608a8adea8402f181d5be
Rares Pintilii <blackknife000@gmail.com>
2020-03-23 16:39:59 +0200

    Lab 3

---
+++ ModelDesignFirst_L1/Model2.edmx.sql
@@ -0,0 +1,93 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/11/2020 16:52:42
+-- Generated from EDMX file: C:\Users\Pintilii\source\repos\ModelDesignFirst_L1\ModelDesignFirst_L1\Model2.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [TestPerson];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_CustomerOrder1]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_CustomerOrder1];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
+GO
+IF OBJECT_ID(N'[dbo].[Orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Orders];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [CustomerId] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(50)  NOT NULL,
+    [City] nvarchar(20)  NOT NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderId] int IDENTITY(1,1) NOT NULL,
+    [TotalValue] decimal(18,0)  NOT NULL,
+    [Date] datetime  NOT NULL,
+    [CustomerCustomerId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [CustomerId] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC);
+GO
+
+-- Creating primary key on [OrderId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [CustomerCustomerId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK_CustomerOrder1]
+    FOREIGN KEY ([CustomerCustomerId])
+    REFERENCES [dbo].[Customers]
+        ([CustomerId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CustomerOrder1'
+CREATE INDEX [IX_FK_CustomerOrder1]
+ON [dbo].[Orders]
+    ([CustomerCustomerId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ ModelDesignFirst_L1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelDesignFirst_L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Model Designer First");
+            TestOneToMany();
+
+            Console.ReadKey();
+        }
+
+        public partial class ModelOneToManyContainer : DbContext
+        {
+            public ModelOneToManyContainer()
+                : base("name=ModelOneToManyContainer")
+            { }
+
+            protected override void OnModelCreating(
+                DbModelBuilder modelBuilder)
+            {
+                throw new UnintentionalCodeFirstException();
+            }
+
+            public virtual DbSet<Customer> Customers { get; set; }
+            public virtual DbSet<Orders> Orders { get; set; } 
+        }
+
+        static void TestPerson()
+        {
+            using (Model1Container context = new Model1Container())
+            {
+                Person p = new Person()
+                {
+                    FirstName = "Julie",
+                    LastName = "Andrew",
+                    MidleName = "T",
+                    TelephonNumber = "1234567890"
+                };
+                context.People.Add(p);
+                context.SaveChanges();
+                var items = context.People;
+                foreach (var x in items)
+                    Console.WriteLine("{0} {1}", x.Id, x.FirstName);
+            }
+        }
+
+        static void TestOneToMany()
+        {
+            Console.WriteLine("One to many association");
+            using (ModelOneToManyContainer context = 
+                new ModelOneToManyContainer())
+            {
+                Customer c = new Customer()
+                {
+                    Name = "Customer 1",
+                    City = "Iasi"
+                };
+
+                Orders o1 = new Orders()
+                {
+                    TotalValue = 200,
+                    Date = DateTime.Now,
+                    Customer = c
+                };
+
+                Orders o2 = new Orders()
+                {
+                    TotalValue = 300,
+                    Date = DateTime.Now,
+                    Customer = c
+                };
+
+                context.Customers.Add(c);
+                context.Orders.Add(o1);
+                context.Orders.Add(o2);
+                context.SaveChanges();
+
+                var items = context.Customers;
+                foreach(var x in items)
+                {
+                    Console.WriteLine("Customer : {0}, {1}, {2}", x.CustomerId, x.Name, x.City);
+                    foreach (var ox in x.Orders)
+                        Console.WriteLine("\tOrders: {0}, {1}, {2}", ox.OrderId, ox.Date, ox.TotalValue);
+                }
+            }
+        }
+    }
+}
